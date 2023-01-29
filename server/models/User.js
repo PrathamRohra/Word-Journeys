ddbbf48756bf81f94ed2dc131f2e0454d874b940
Pratham Rohra <99543617+PrathamRohra@users.noreply.github.com>
2023-01-29 05:32:43 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 /*MONGOOSE CONNECTION*/
-mongoose.connect('mongodb+srv://pratham123:pratham123@cluster0.muip1om.mongodb.net/?retryWrites=true&w=majority')
+
 
 const UserSchema = new mongoose.Schema({
     userName: {
